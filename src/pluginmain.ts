458aae33ae383b2d795c219f27bade210bdaea52
Voter-Science <Info@Voter-Science.com>
2018-01-02 16:53:13 -0800

    Update to use new NPM packages

--- src/pluginmain.ts
+++ src/pluginmain.ts
@@ -6,11 +6,18 @@
 // - basic scaffolding for error reporting. 
 // This calls TRC APIs and binds to specific HTML elements from the page.  
 
-import * as trc from 'trclib/trc2';
-import * as trcplugin from 'trclib/plugin';
-import * as trchtml from 'trclib/trchtml';
-import * as gps from 'trclib/gps';
-import * as trcFx from 'trclib/trcfx'; 
+import * as XC from 'trc-httpshim/xclient'
+import * as common from 'trc-httpshim/common'
+
+import * as core from 'trc-core/core'
+
+import * as trcSheet from 'trc-sheet/sheet' 
+import * as trcSheetEx from 'trc-sheet/sheetEx'
+
+import * as gps from 'trc-web/gps'
+import * as plugin from 'trc-web/plugin'
+import * as trchtml from 'trc-web/html'
+
 
 declare var $: any; // external definition for JQuery 
 
@@ -19,46 +26,45 @@ declare var $: any; // external definition for JQuery
 declare var showError : (error:any) => void; // error handler defined in index.html
 
 export class MyPlugin {
-    private _sheet: trc.Sheet;
-    private _gps : gps.IGpsTracker;
-    private _options : trcplugin.PluginOptionsHelper;
+    private _sheet: trcSheet.SheetClient;
+    private _pluginClient : plugin.PluginClient;
+    private _gps : common.IGeoPointProvider;
 
-    // Entry point called from browser. 
-    // This creates real browser objects and passes into the ctor. 
-    // Whereas a unit test would skip this and call the ctor directly. 
     public static BrowserEntryAsync(
-        sheet: trc.ISheetReference, 
-        opts : trcplugin.IPluginOptions
+        auth: plugin.IStart,
+        opts : plugin.IPluginOptions
     ) : Promise<MyPlugin> {
-        var trcSheet = new trc.Sheet(sheet);                
-        var opts2 = trcplugin.PluginOptionsHelper.New(opts, trcSheet);
-
-        // Track GPS location of device
+        
+        // You can set gpsTracker null if you don't need GPS. 
         var gpsTracker = new gps.GpsTracker(); // Only works in browser
-        gpsTracker.start(null); // ignore callback
+        var pluginClient = new plugin.PluginClient(auth,opts, gpsTracker);
 
         // Do any IO here...
         
-        var throwError =false;
-
-        var plugin = new MyPlugin(trcSheet, opts2, gpsTracker);
-        return plugin.InitAsync().then( () => {
+        var throwError =false; // $$$ remove this
+        
+        var plugin2 = new MyPlugin(pluginClient);
+        plugin2._gps = gpsTracker;
+        return plugin2.InitAsync().then( () => {
             if (throwError) {
                 throw "some error";
             }
-            return plugin;                        
+            
+            gpsTracker.start((loc) => plugin2.OnGpsChanged(loc)); // ignore callback
+            return plugin2;                        
         });
     }
 
+    private OnGpsChanged(loc : gps.IGeoPoint) : void {
+        // Notification that the GPS position has change. 
+        // Use this if you have a map view showing the user's "current location"
+        var msg = "(Lat: " + loc.Lat + ", Long:" + loc.Long + ")";
+        $("#locInfo").text(msg);
+    }
+
     // Expose constructor directly for tests. They can pass in mock versions. 
-    public constructor(
-        sheet: trc.Sheet,
-        opts2 : trcplugin.PluginOptionsHelper, 
-        gpsTracker : gps.IGpsTracker
-    ) {
-        this._sheet = sheet; // Save for when we do Post
-        this._options = opts2;               
-        this._gps = gpsTracker;    
+    public constructor(p : plugin.PluginClient) {
+        this._sheet = new trcSheet.SheetClient(p.HttpClient, p.SheetId);
     }
     
 
@@ -71,7 +77,7 @@ export class MyPlugin {
     }
 
     // Display sheet info on HTML page
-    public updateInfo(info: trc.ISheetInfoResult): void {
+    public updateInfo(info: trcSheet.ISheetInfoResult): void {
         $("#SheetName").text(info.Name);
         $("#ParentSheetName").text(info.ParentName);
         $("#SheetVer").text(info.LatestVersion);
@@ -91,15 +97,16 @@ export class MyPlugin {
             catch(showError);
     }
 
+
     // downloading all contents and rendering them to HTML can take some time. 
     public onGetSheetContents(): void {
         trchtml.Loading("contents");
         //$("#contents").empty();
         //$("#contents").text("Loading...");
 
-        trcFx.SheetEx.InitAsync(this._sheet, this._gps, (sheetEx)=>
+        trcSheetEx.SheetEx.InitAsync(this._sheet, this._gps).then((sheetEx)=>
         {
-            this._sheet.getSheetContentsAsync().then((contents) => {
+            return this._sheet.getSheetContentsAsync().then((contents) => {
                 var render = new trchtml.SheetControl("contents", sheetEx);
                 // could set other options on render() here
                 render.render();
